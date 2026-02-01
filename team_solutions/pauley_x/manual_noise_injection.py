c4d57f75580502e9b7e7f0adcb4aeadd0aa83b39
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2026-02-01 03:07:24 -0500

    wip poi noise injection

--- team_solutions/pauley_x/manual_noise_injection.py
+++ team_solutions/pauley_x/manual_noise_injection.py
@@ -1,10 +1,20 @@
 from bloqade import squin
+from typing import Tuple, Callable, Any, Optional
 
-payload = {
-    "a": (squin.depolarize, 0.5),
+
+NOISE_CHANNELS = {
+    "depolarize": (squin.depolarize, 0.5),
+    "amplitude_damping": (squin.qubit_loss, 0.5),
+    # can add more
 }
 
-def poi(q, label):
+### Example payload
+poi_noises_payload = {
+    "a": (squin.depolarize, 0.5),
+    "interesting_area": (squin.qubit_loss, 0.5)
+}
+
+def poi(q, label, custom_probability: Optional[float] = None):
     """
     Implements a 'point of interest' gate.
 
@@ -14,9 +24,10 @@ def poi(q, label):
         noise_channel: noise channel.
     """
 
-    noise_channel, probability = payload.get(label)
-    return noise_channel(p=probability, qubit=q)
-
+    noise_channel, default_prob = poi_noises_payload.get(label, (None, None))
+    probability = custom_probability if custom_probability is not None else default_prob
+    if noise_channel is not None:
+        return noise_channel(p=probability, qubit=q)
 
 
 
